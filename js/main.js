7c377934ecd607d2d2e8a21298513d88cc9924d5
sandfox11 <samstebbing89@gmail.com>
2025-11-07 17:09:33 -0600

    Add Updates page and update navigation buttons
    Introduces Updates/index.html to display site updates. Refactors button IDs in main/index.html and js/main.js for consistency, adds navigation to the new Updates page, and separates About and Updates buttons for clearer user interaction.

--- js/main.js
+++ js/main.js
@@ -1,7 +1,12 @@
 console.log("main.js successfully loaded");
 
-let AboutBTN = document.getElementById("about");
+let AboutBTN = document.getElementById("AboutBTN");
+let UpdatesBTN = document.getElementById("UpdatesBTN");
 
 AboutBTN.addEventListener('click', () => {
   window.location.href = "../about/index.html";
+});
+
+UpdatesBTN.addEventListener('click', () => {
+  window.location.href = "../Updates/index.html";
 });
