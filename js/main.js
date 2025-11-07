6ab2bf1709984ba76bd8c3a1cb7108fedee5318d
Sandfox11 <samstebbing89@gmail.com>
2025-11-07 16:37:09 -0600

    Add event listeners for About and Back buttons

--- js/main.js
+++ js/main.js
@@ -1,7 +1,12 @@
 console.log("main.js successfully loaded");
 
 let AboutBTN = document.getElementById("about");
+let AboutBack = document.getElementByID("about-BackBTN");
 
 AboutBTN.addEventListener('click', () => {
   window.location.href = "../about/index.html";
+});
+
+AboutBack.addEventListener('click', () => {
+  window.location.href = "../main/index.html";
 });
