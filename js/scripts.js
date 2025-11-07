b2b0e956e02d3214e4500930b2071c62da18afb7
sandfox11 <samstebbing89@gmail.com>
2025-11-06 18:23:20 -0600

    pass check

--- js/scripts.js
+++ js/scripts.js
@@ -1,1 +1,11 @@
-console.log("Test");
+console.log("JS loaded");
+let password = document.getElementById('Pass');
+password.addEventListener('keydown', (e) => {
+  if (e.key === 'Enter') {
+  e.preventDefault();
+  console.log("Text input submitted:", password.value);
+  if (password.value === "pass123") {
+    window.location.href = "../index.html";
+  }
+  }
+});
