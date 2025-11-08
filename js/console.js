7bbff0cdec8892073e367ea979066c6e5abc67ad
Sandfox11 <samstebbing89@gmail.com>
2025-11-07 23:17:31 -0600

    Add send button functionality to send text

--- js/console.js
+++ js/console.js
@@ -2,10 +2,17 @@ console.log("console.js successfully loaded");
 
 let CNSL = document.getElementById("ConINPT");
 let History = document.getElementById("history");
+let send = document.getElementById("sendBTN");
+
+send.addEventListener('click', SendText();)
 
 CNSL.addEventListener('keydown', (e) => {
   if (e.key === "enter") {
-    History.innerHTML = CNSL.value;
-    CNSL.value = "";
+    SendText();
   }
 });
+
+let SendText = function () {
+  History.innerHTML = CNSL.value;
+  CNSL.value = "";
+};
