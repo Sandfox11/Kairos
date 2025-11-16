f3b4f7ac1049e0159ce20c05b81316cedbbda01c
sandfox11 <samstebbing89@gmail.com>
2025-11-15 21:40:48 -0600

    Add Leads and Tools pages with navigation
    Introduces new leads and tools pages, updates main page buttons to link to these sections, and adds corresponding event listeners in main.js for navigation. Both new pages display a placeholder and a back button to return to the main page.

--- js/main.js
+++ js/main.js
@@ -2,6 +2,8 @@ console.log("main.js successfully loaded");
 
 let AboutBTN = document.getElementById("AboutBTN");
 let UpdatesBTN = document.getElementById("UpdatesBTN");
+let LeadsBTN = document.getElementById("LeadsBTN");
+let ToolsBTN = document.getElementById("ToolsBTN");
 
 AboutBTN.addEventListener('click', () => {
   window.location.href = "../about/index.html";
@@ -9,4 +11,12 @@ AboutBTN.addEventListener('click', () => {
 
 UpdatesBTN.addEventListener('click', () => {
   window.location.href = "../Updates/index.html";
+});
+
+LeadsBTN.addEventListener('click', () => {
+  window.location.href = "../leads/index.html";
+});
+
+ToolsBTN.addEventListener('click', () => {
+  window.location.href = "../tools/index.html";
 });
